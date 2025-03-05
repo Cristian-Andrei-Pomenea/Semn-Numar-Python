41f751489154cb0e7dd7a73d6c391d1f12558480
Pomenea Cristian-Andrei <cristian.pomenea@stud.acs.upb.ro>
2025-03-05 13:57:04 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,8 @@
+numar = float(input("Introducet un numar: "))
+
+if numar > 0:
+    print("Numarul este pozitiv.")
+elif numar < 0:
+    print("Numarul este negativ.")
+else:
+    print("Numarul este 0.")
